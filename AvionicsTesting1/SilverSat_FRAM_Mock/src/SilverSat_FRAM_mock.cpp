f24735fb637dd8f0e54edbc8d9e43f6616abb7e7
Lee Congdon <lee@lcongdon.com>
2022-07-10 07:02:57 -0400

    Update documentation

--- AvionicsTesting1/SilverSat_FRAM_Mock/src/SilverSat_FRAM_mock.cpp
+++ AvionicsTesting1/SilverSat_FRAM_Mock/src/SilverSat_FRAM_mock.cpp
@@ -1,43 +1,51 @@
-//
-// Mock FRAM for demonstration and testing
-// Lee A. Congdon
-// 22 June 2022
-//
-// Based on Adafruit_FRAM_I2C
-//
+/**
+ * @file SilverSat_FRAM_mock.cpp
+ * @author Lee A. Congdon (lee@silversat.org)
+ * @brief Mock FRAM for demonstration and testing
+ * @version 1.0.0
+ * @date 2022-06-22
+ *
+ *
+ */
 
 #include "SilverSat_FRAM_mock.h"
 
-// Constructor
+// @brief Construct a new FRAM object
 
-FRAM::FRAM() {
+FRAM::FRAM()
+{
     _i2caddr = FRAM_DEFAULT_ADDRESS;
 }
 
 // Start the session
 
-bool FRAM::begin(uint8_t i2caddr, void* theWire) {
+bool FRAM::begin(uint8_t i2caddr, void *theWire)
+{
     _i2caddr = i2caddr;
     return true;
 }
 
 // Write a byte
 
-bool FRAM::write(uint16_t addr, uint8_t value) {
+bool FRAM::write(uint16_t addr, uint8_t value)
+{
     _memory[addr % _memory_size] = value;
     return true;
 }
 
 // Read a byte
 
-uint8_t FRAM::read(uint16_t addr) {
+uint8_t FRAM::read(uint16_t addr)
+{
     return _memory[addr % _memory_size];
 }
 
 // Write multiple bytes
 
-bool FRAM::write(uint16_t addr, uint8_t *buffer, uint16_t num) {
-    while (num--) {
+bool FRAM::write(uint16_t addr, uint8_t *buffer, uint16_t num)
+{
+    while (num--)
+    {
         write(addr++, buffer[0]);
         buffer++;
     }
@@ -46,8 +54,10 @@ bool FRAM::write(uint16_t addr, uint8_t *buffer, uint16_t num) {
 
 // Read multiple bytes
 
-bool FRAM::read(uint16_t addr, uint8_t *buffer, uint16_t num) {
-    for (uint16_t i = 0; i < num; i++) {
+bool FRAM::read(uint16_t addr, uint8_t *buffer, uint16_t num)
+{
+    for (uint16_t i = 0; i < num; i++)
+    {
         buffer[i] = read(addr);
         addr++;
     }
