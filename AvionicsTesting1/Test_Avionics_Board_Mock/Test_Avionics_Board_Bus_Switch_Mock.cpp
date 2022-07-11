3b0e8ecc84597bb8da631f9557dbf8881bfe3ff3
Lee Congdon <lee@lcongdon.com>
2022-07-11 08:54:02 -0400

    Board, SAMD21, Watchdog, I2C, Bus Switch

---
+++ AvionicsTesting1/Test_Avionics_Board_Mock/Test_Avionics_Board_Bus_Switch_Mock.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file Test_Avionics_Board_Bus_Switch_Mock.cpp
+ * @author Lee A. Congdon (lee@silversat.org)
+ * @brief Test the I2C bus switch
+ * @version 1.0.0
+ * @date 2022-07-11
+ *
+ *
+ */
+
+#include <SilverSat_Bus_Switch_mock.h>
+
+// Run the tests
+
+bool test_Bus_Switch()
+{
+
+    BusSwitch bus_switch;
+
+    Serial.println("-----");
+    Serial.println("Starting Bus Switch Test");
+
+    // Test the bus enable and disable functions
+
+    for (auto index = 1; index < 21; index++)
+    {
+        bus_switch.enableBus();
+        bus_switch.disableBus();
+        bus_switch.enableBus();
+    }
+
+    // Dump the debug log
+
+    auto count = bus_switch.getLogSize();
+    Serial.print("There are ");
+    Serial.print(count);
+    Serial.println(" entries in the log");
+    for (auto index = count; index > 0; index--)
+    {
+        auto log_entry = bus_switch.getLogEntry();
+        Serial.print("Entry: ");
+        switch (log_entry.action)
+        {
+        case BusSwitch::Action::create:
+            Serial.print("Created switch at ");
+            break;
+        case BusSwitch::Action::enable:
+            Serial.print("Enabled switch at ");
+            break;
+        case BusSwitch::Action::disable:
+            Serial.print("Disabled switch at ");
+            break;
+        }
+        Serial.println(log_entry.time_stamp);
+    }
+
+    // End of test
+
+    Serial.println("\nBus Switch test complete");
+    return true;
+}
---
+++ AvionicsTesting1/Test_Avionics_Board_Mock/Test_Avionics_Board_I2C_Mock.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file Test_Avionics_Board_I2C_Mock.cpp
+ * @author Lee A. Congdon (lee@silversat.org)
+ * @brief Test the I2C connection
+ * @version 1.0.0
+ * @date 2022-07-11
+ *
+ *
+ */
+
+// Based on:
+// Wire Master Reader
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Reads data from an I2C/TWI slave device
+// Refer to the "Wire Slave Sender" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+#include <SilverSat_I2C_mock.h>
+
+// Run the tests
+
+bool test_I2C()
+{
+
+  I2C i2c;
+
+  Serial.println("-----");
+  Serial.println("Starting I2C Test");
+
+  // Test read
+
+  i2c.begin(); // join i2c bus (address optional for master)
+  Serial.println("Reading I2C bus");
+  for (auto tests = 10; tests > 0; tests--)
+  {
+    i2c.requestFrom(2, 6, true); // request 6 bytes from slave device #2
+
+    for (auto quantity = i2c.available(); quantity > 0; quantity--) // slave may send less than requested
+    {
+      char c = i2c.read(); // receive a byte as character
+      Serial.print(c);     // print the character
+    }
+    Serial.print("\n");
+  }
+
+  // End of test
+
+  Serial.println("\nI2C Test Complete");
+  return true;
+}
