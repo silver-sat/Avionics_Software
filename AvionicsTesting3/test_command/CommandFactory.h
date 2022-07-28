ae73747bb13330c28737332875bcbace0cc70a20
Lee Congdon <lee@lcongdon.com>
2022-07-28 18:11:18 -0400

    Refactor command factory

---
+++ AvionicsTesting3/test_command/CommandFactory.h
@@ -0,0 +1,64 @@
+/**
+ * @file CommandFactory.h
+ * @author Lee A. Congdon (lee@silversat.org)
+ * @brief Generate the appropriate command object
+ * @version 1.0.0
+ * @date 2022-07-28
+ *
+ *
+ */
+
+#ifndef COMMANDFACTORY_H
+#define COMMANDFACTORY_H
+
+#include "Command.h"
+
+/**
+ * @brief Factory for Command objects
+ *
+ */
+class CommandFactory
+{
+public:
+    /**
+     * @brief Construct a new Command Factory object
+     *
+     */
+    CommandFactory();
+    /**
+     * @brief Construct a new Command Factory object
+     *
+     * @param command operation
+     */
+    CommandFactory(String command);
+    /**
+     * @brief Construct a new Command Factory object
+     *
+     * @param command operation
+     * @param parameter
+     */
+    CommandFactory(String command, int parameter);
+    /**
+     * @brief Construct a new Command Factory object
+     *
+     * @param command operation
+     * @param time time value
+     */
+    CommandFactory(String command, time_value time);
+    /**
+     * @brief Destroy the Command Factory object
+     *
+     */
+    ~CommandFactory();
+    /**
+     * @brief Get the command object
+     *
+     * @return Command*
+     */
+    Command *get_command();
+
+private:
+    Command *_command;
+};
+
+#endif
