d1bc8f75846339940a805b2b6209d2f7f8f41ce2
Lee Congdon <lee@lcongdon.com>
2022-10-03 09:42:54 -0400

    October 15th

---
+++ Software for Meetings/rollover/rollover.ino
@@ -0,0 +1,87 @@
+/**
+ * @file rollover.ino
+ * @author Lee A. Congdon (lee@silversat.org)
+ * @brief Demonstrate rollover of counter
+ * @version 1.0.0
+ * @date 2022-10-03
+ *
+ *
+ */
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial)
+    {
+    }
+
+    // Difference before rollover
+
+    uint8_t counter{'\xFE'};
+    uint8_t start{'\xEE'};
+    uint8_t difference{counter - start};
+
+    Serial.println("Before rollover");
+    Serial.print("counter: ");
+    Serial.print(counter);
+    Serial.print(" (0x");
+    Serial.print(counter, HEX);
+    Serial.println(")");
+    Serial.print("start: ");
+    Serial.print(start);
+    Serial.print(" (0x");
+    Serial.print(start, HEX);
+    Serial.println(")");
+    Serial.print("difference: ");
+    Serial.print(difference);
+    Serial.print(" (0x");
+    Serial.print(difference, HEX);
+    Serial.println(")");
+
+    // Difference with rollover
+
+    counter += 15;
+    difference = counter - start;
+
+    Serial.println("After rollover");
+    Serial.print("counter: ");
+    Serial.print(counter);
+    Serial.print(" (0x");
+    Serial.print(counter, HEX);
+    Serial.println(")");
+    Serial.print("start: ");
+    Serial.print(start);
+    Serial.print(" (0x");
+    Serial.print(start, HEX);
+    Serial.println(")");
+    Serial.print("difference: ");
+    Serial.print(difference);
+    Serial.print(" (0x");
+    Serial.print(difference, HEX);
+    Serial.println(")");
+
+    // Difference after start updated
+
+    start = counter;
+    counter += 17;
+    difference = counter - start;
+
+    Serial.println("After start updated");
+    Serial.print("counter: ");
+    Serial.print(counter);
+    Serial.print(" (0x");
+    Serial.print(counter, HEX);
+    Serial.println(")");
+    Serial.print("start: ");
+    Serial.print(start);
+    Serial.print(" (0x");
+    Serial.print(start, HEX);
+    Serial.println(")");
+    Serial.print("difference: ");
+    Serial.print(difference);
+    Serial.print(" (0x");
+    Serial.print(difference, HEX);
+    Serial.println(")");
+}
+
+void loop() {}
