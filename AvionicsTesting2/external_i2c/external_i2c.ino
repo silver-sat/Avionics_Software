ca78499561003b151c78dcf421df2b09dd403bd3
bscohen06 <104695188+bscohen06@users.noreply.github.com>
2022-11-12 16:20:39 -0500

    Update external_i2c.ino

--- AvionicsTesting2/external_i2c/external_i2c.ino
+++ AvionicsTesting2/external_i2c/external_i2c.ino
@@ -8,15 +8,16 @@ void setup(){
 pinMode(en_ext_I2C, OUTPUT);
 pinMode(SDA_NON_CRIT, OUTPUT);
 pinMode(SCL_NON_CRIT, OUTPUT);
+ //turns pins on
+digitalWrite(en_ext_I2C, HIGH);
+digitalWrite(SDA_NON_CRIT, HIGH);
+digitalWrite(SCL_NON_CRIT, HIGH);
 }
-//turns on and off every half second
+
 void loop(){
-  digitalWrite(en_ext_I2C, HIGH);
- digitalWrite(SDA_NON_CRIT, HIGH);
- digitalWrite(SCL_NON_CRIT, HIGH);
-  delay(500);
- digitalWrite(en_ext_I2C, LOW);
- digitalWrite(SDA_NON_CRIT, LOW);
- digitalWrite(SCL_NON_CRIT, LOW);
-  delay(500);
+//   delay(500);
+//  digitalWrite(en_ext_I2C, LOW);
+//  digitalWrite(SDA_NON_CRIT, LOW);
+//  digitalWrite(SCL_NON_CRIT, LOW);
+//   delay(500);
 }
