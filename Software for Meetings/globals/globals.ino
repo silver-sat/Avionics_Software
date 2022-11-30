00f25b85dede006a6a7e23538b1bc217ed07bff1
Lee Congdon <lee@lcongdon.com>
2022-11-30 11:00:30 -0500

    December 3

--- Software for Meetings/globals/globals.ino
+++ Software for Meetings/globals/globals.ino
@@ -10,22 +10,28 @@
 int a{ 20 };
 int b{ 22 };
 
+// the local variables a and b shadow the global variables
 int add(int a, int b) {
   a = 10;
   b = 5;
   return a + b;
 }
 
-
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
+
+// global variables a and b
   Serial.print("a: ");
   Serial.print(a);
   Serial.print(" b: ");
   Serial.println(b);
+  
+// call the add function with copies of the global variables
   Serial.print("sum: ");
   Serial.println(add(a, b));
+  
+// the global variables are unchanged by the assignments in add
   Serial.print("a: ");
   Serial.print(a);
   Serial.print(" b: ");
