127c875545c0adc0df75bfbc901fa149be23212b
Lee Congdon <lee@lcongdon.com>
2023-01-06 15:11:29 -0500

    January 7

--- Software for Meetings/sums.cpp
+++ Software for Meetings/sums.cpp
@@ -15,45 +15,32 @@
 int main()
 {
 
-    /**
-     * @brief C-style loop
-     *
-     * Traditional, standard Arduino
-     */
+    // Traditional C-style loop
 
-    int array1[5] = {4, 7, 2, 5, 1};
-    size_t length = sizeof(array1) / sizeof(array1[0]);
+    constexpr size_t array_size{5};
+    constexpr int array1[array_size] = {4, 7, 2, 5, 1};
     int sum1 = 0;
 
-    for (size_t i = 0; i < length; i++)
+    for (size_t i = 0; i < array_size; i++)
     {
-        sum1 += array1[i];
+        sum1 = sum1 + array1[i];
     }
 
     printf("Sum 1 is: %d\n", sum1);
 
-    /**
-     * @brief for-each loop
-     *
-     * Preferred, avoids off by one and indexing errors
-     * Supported by Arduino
-     */
+    // For-each loop (avoids off-by-one and indexing errors)
 
-    int array2[]{4, 7, 2, 5, 1};
+    constexpr int array2[]{4, 7, 2, 5, 1};
     int sum2{0};
 
-    for (int number : array2)
+    for (auto number : array2)
     {
         sum2 += number;
     }
 
     std::cout << "Sum 2 is: " << sum2 << '\n';
 
-    /**
-     * @brief Library function
-     *
-     * Available in environments supporting the standard library 
-     */
+    // Library function (available in environments supporting the standard library)
 
     std::array array3{4, 7, 2, 5, 1};
     std::cout << "Sum 3 is: " << std::accumulate(array3.begin(), array3.end(), 0) << '\n';
