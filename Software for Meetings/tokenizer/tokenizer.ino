b8a6b986340bba2e31936076db6d1e31c1094189
Lee Congdon <lee@lcongdon.com>
2023-05-01 14:11:20 -0400

    Update formatting

--- Software for Meetings/tokenizer/tokenizer.ino
+++ Software for Meetings/tokenizer/tokenizer.ino
@@ -9,44 +9,36 @@
  *
  */
 
-constexpr size_t maximum_tokens{10}; // Allow up to 10 tokens
+constexpr size_t maximum_tokens{ 10 };  // Allow up to 10 tokens
 
 /**
  * @brief Parse the command string into tokens
  *
- * @param command string to be parsed
- * @param tokens String array of tokens
- * @param token_count token count, -1 if error
+ * @param[in] command string to be parsed
+ * @param[out] tokens String array of tokens
+ * @param[out] token_count token count, -1 if error
  *
  */
 
-void tokenizer(const String &command, String tokens[], size_t &token_count)
-{
-    for (size_t index{0}; index < maximum_tokens; ++index)
-        tokens[index] = "";
-    token_count = 0;
-    bool in_token{false};
-    for (auto character : command)
-    {
-        if (character == ' ')
-        {
-            if (in_token)
-                in_token = false;
+void tokenizer(const String &command, String tokens[], size_t &token_count) {
+  for (size_t index{ 0 }; index < maximum_tokens; ++index)
+    tokens[index] = ""; // clear the output tokens
+  token_count = 0;
+  bool in_token{ false };
+  for (auto character : command) {
+    if (character == ' ') {
+      in_token = false; // tokens do not contain blanks
+    } else {
+      if (!in_token) {
+        if (++token_count > maximum_tokens) {
+          token_count = -1;  // too many tokens
+          break;
         }
-        else
-        {
-            if (!in_token)
-            {
-                if (++token_count > maximum_tokens)
-                {
-                    token_count = -1; // too many tokens
-                    break;
-                }
-                in_token = true;
-            }
-            tokens[token_count - 1] += character;
-        }
+        in_token = true;
+      }
+      tokens[token_count - 1] += character;
     }
+  }
 }
 
 /**
@@ -58,20 +50,16 @@ void tokenizer(const String &command, String tokens[], size_t &token_count)
  *
  */
 
-void print_result(const String &command, const String tokens[], const size_t token_count)
-{
-    Serial.println("For command: \"" + command + '\"');
-    if (static_cast<int>(token_count) == -1)
-    {
-        Serial.println("Parsing error");
-    }
-    else
-    {
-        Serial.println("Token count is " + String(token_count));
-        for (size_t index{0}; index < token_count; ++index)
-            Serial.println(tokens[index]);
-    }
-    Serial.println();
+void print_result(const String &command, const String tokens[], const size_t token_count) {
+  Serial.println("For command: \"" + command + '\"');
+  if (static_cast<int>(token_count) == -1) {
+    Serial.println("Parsing error");
+  } else {
+    Serial.println("Token count is " + String(token_count));
+    for (size_t index{ 0 }; index < token_count; ++index)
+      Serial.println(tokens[index]);
+  }
+  Serial.println();
 }
 
 /**
@@ -79,55 +67,58 @@ void print_result(const String &command, const String tokens[], const size_t tok
  *
  */
 
-void setup()
-{
-    Serial.begin(115200);
-    while (!Serial)
-        ;
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 
-    String tokens[maximum_tokens];
-    size_t token_count{};
-    String test_command;
+  String tokens[maximum_tokens];
+  size_t token_count{};
+  String test_command;
 
-    test_command = "NoOperate";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "NoOperate";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     11";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     11";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     11 12  ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     11 12  ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "SetClock 2023 11 11 10 10 10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "SetClock 2023 11 11 10 10 10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "SetClock   2023   11   11   10   10   10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "SetClock   2023   11   11   10   10   10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "     NoOperate";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "     NoOperate";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "     SetClock 2023 11 11 10 10   10      ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "     SetClock 2023 11 11 10 10   10      ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
+
+  test_command = "     ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 }
 
 /**
--- Software for Meetings/tokenizer2/tokenizer2.ino
+++ Software for Meetings/tokenizer2/tokenizer2.ino
@@ -9,7 +9,7 @@
  *
  */
 
-constexpr size_t maximum_tokens{10};
+constexpr size_t maximum_tokens{ 10 };
 
 /**
  * @brief Parse command for blank-delimited tokens
@@ -20,31 +20,30 @@ constexpr size_t maximum_tokens{10};
  *
  */
 
-void tokenizer(const String &command, String tokens[], size_t &token_count)
-{
-    String trimmed_command{command};
-    size_t token_index{0};
-    while (trimmed_command.length() > 0)
-    {
-        if (token_index >= maximum_tokens)
-        {
-            token_count = -1; // too many tokens
-            return;
-        }
-        trimmed_command.trim();
-        int next_blank{trimmed_command.indexOf(' ')};
-        if (next_blank == -1)
-        { // no more blanks in command
-            tokens[token_index++] = trimmed_command;
-            trimmed_command = "";
-        }
-        else
-        {
-            tokens[token_index++] = trimmed_command.substring(0, next_blank);
-            trimmed_command = trimmed_command.substring(next_blank);
-        }
+void tokenizer(const String &command, String tokens[], size_t &token_count) {
+
+  token_count = 0;
+  String trimmed_command{ command };
+
+  trimmed_command.trim();  // remove leading (and trailing) blanks
+  while (trimmed_command.length() > 0) {
+
+    if (token_count >= maximum_tokens) {
+      token_count = -1;  // too many tokens
+      return;
     }
-    token_count = token_index;
+
+    int next_blank{ trimmed_command.indexOf(' ') };
+    if (next_blank == -1) {  // no more blanks in command
+      tokens[token_count++] = trimmed_command;
+      trimmed_command = "";
+    } else {
+      tokens[token_count++] = trimmed_command.substring(0, next_blank);
+      trimmed_command = trimmed_command.substring(next_blank);
+    }
+
+    trimmed_command.trim();  // remove leading blank(s)
+  }
 }
 
 /**
@@ -56,20 +55,16 @@ void tokenizer(const String &command, String tokens[], size_t &token_count)
  *
  */
 
-void print_result(const String &command, const String tokens[], const size_t token_count)
-{
-    Serial.println("For command: \"" + command + '\"');
-    if (static_cast<int>(token_count) == -1)
-    {
-        Serial.println("Parsing error");
-    }
-    else
-    {
-        Serial.println("Token count is " + String(token_count));
-        for (size_t index{0}; index < token_count; ++index)
-            Serial.println(tokens[index]);
-    }
-    Serial.println();
+void print_result(const String &command, const String tokens[], const size_t token_count) {
+  Serial.println("For command: \"" + command + '\"');
+  if (static_cast<int>(token_count) == -1) {
+    Serial.println("Parsing error");
+  } else {
+    Serial.println("Token count is " + String(token_count));
+    for (size_t index{ 0 }; index < token_count; ++index)
+      Serial.println(tokens[index]);
+  }
+  Serial.println();
 }
 
 /**
@@ -77,55 +72,58 @@ void print_result(const String &command, const String tokens[], const size_t tok
  *
  */
 
-void setup()
-{
-    Serial.begin(115200);
-    while (!Serial)
-        ;
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 
-    String tokens[maximum_tokens];
-    size_t token_count{};
-    String test_command;
+  String tokens[maximum_tokens];
+  size_t token_count{};
+  String test_command;
 
-    test_command = "NoOperate";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "NoOperate";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     11";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     11";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "Invalid 2 3 4 5 6 7 8 9 10     11 12  ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "Invalid 2 3 4 5 6 7 8 9 10     11 12  ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "SetClock 2023 11 11 10 10 10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "SetClock 2023 11 11 10 10 10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "SetClock   2023   11   11   10   10   10";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "SetClock   2023   11   11   10   10   10";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "     NoOperate";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "     NoOperate";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "     SetClock 2023 11 11 10 10   10      ";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "     SetClock 2023 11 11 10 10   10      ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 
-    test_command = "";
-    tokenizer(test_command, tokens, token_count);
-    print_result(test_command, tokens, token_count);
+  test_command = "";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
+
+  test_command = "     ";
+  tokenizer(test_command, tokens, token_count);
+  print_result(test_command, tokens, token_count);
 }
 
 /**
