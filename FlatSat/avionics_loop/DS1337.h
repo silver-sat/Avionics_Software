a63d7014a2aea18c8cd2d4cf0ba556ccae10f9f2
Lee Congdon <lee@lcongdon.com>
2023-08-30 07:25:21 -0400

    Remove diagnostic pragmas

--- FlatSat/avionics_loop/DS1337.h
+++ FlatSat/avionics_loop/DS1337.h
@@ -15,17 +15,7 @@
 
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Weffc++"
-#pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wunused"
-#pragma GCC diagnostic ignored "-Wconversion"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#pragma GCC diagnostic ignored "-Wnon-virtual-dtor"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wdeprecated-copy"
 #include <RTClib.h>
-#pragma GCC diagnostic pop
 
 // todo: resolve object creation location and use address constant
 constexpr unsigned RTC_I2C_ADDRESS{0x68};   /**< realtime clock I2C address @hideinitializer */
